000f7b91bd40fdaf8404ccd1d773ed5438503247
Marmarossa <Mario.kodsi.94@gmail.com>
2024-02-11 10:51:21 +0100

    update readme with a description

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+from collections import OrderedDict
+
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_word_count(text)
     print(f"There are {num_words} words in this text")
+    num_letters = get_letter_count(text)
+    print(num_letters)
 
 def get_book_text(path):
     with open(path) as f:
@@ -16,7 +20,24 @@ def get_word_count(text):
     words = text.split()
     return len(words)
 
+def sort_on(dict):
+    return dict["num"]
 
+def get_letter_count(text):
+    dict_letters = {}
+    lower_text= text.lower()
+    for letter in lower_text:
+        if letter.isalpha() == True:
+            if letter in dict_letters:
+                dict_letters[letter] += 1
+            else:
+                dict_letters[letter] = 1
+
+    ordered_dict = OrderedDict(sorted(dict_letters.items(), key=lambda t: t[0]))
+
+    
+    return ordered_dict
+        
 
 
 main()
--- test.py
+++
@@ -1,9 +0,0 @@
-def get_letter_count(words):
-    lower_text = words.lower()
-    num_letters = [x for x in lower_text]
-    return num_letters
-
-test = "I love banana"
-get_letter_count(test)
-num_letter = [x for x in test]
-print(num_letter)
