b73550a1f6458b8d8487c8eea4c741c376309880
Marmarossa <Mario.kodsi.94@gmail.com>
2024-11-06 21:50:31 +0100

    refactored code + added Count_characters

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 def main():
-    with open("github.com/Marmarossa/bookbot/books/frankenstein.txt") as f:
+    path = "github.com/Marmarossa/bookbot/books/frankenstein.txt"
+    text = get_book_path(path)
+    num_words = count_words(text)
+    print(count_characters(text))
+
+
+def count_characters(text):
+    lower_text = text.lower()
+    char_dict = {}
+    for char in lower_text:
+        if char in char_dict:
+            char_dict[char] += 1
+        else:
+            char_dict[char] = 1
+    return char_dict
+
+def count_words(text):
+    words = text.split()
+    return len(words)
+
+def get_book_path(path):
+    with open(path) as f:
         file_contents = f.read()
+    return file_contents
 
-    def count_words():
-        
-        words = file_contents.split()
-        return len(words)
-
-    print(count_words())
 main()
